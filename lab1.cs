8bea5692b6b37176a8f7e6efacb515cb9bc405d6
spaluszkiewicz04 <stanislaw.paluszkiewicz@student.put.poznan.pl>
2026-03-05 14:28:20 +0100

    First Commit

---
+++ lab1.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace HelloWorld
+{
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
