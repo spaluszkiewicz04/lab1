f3394a58ab105db83ea7040727b9cbdd1cb91f1c
spaluszkiewicz04 <stanislaw.paluszkiewicz@student.put.poznan.pl>
2026-03-05 14:42:12 +0100

    test

--- lab1.cs
+++ lab1.cs
@@ -2,11 +2,11 @@ using System;
 
 namespace HelloWorld
 {
-    class Program1
+    class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
     }
-}
+}   
