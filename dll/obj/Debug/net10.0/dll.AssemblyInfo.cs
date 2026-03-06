652b31f40166b629503175e756526e2f11790765
spaluszkiewicz04 <stanislaw.paluszkiewicz@student.put.poznan.pl>
2026-03-06 23:51:53 +0100

    K

--- dll/obj/Debug/net10.0/dll.AssemblyInfo.cs
+++ dll/obj/Debug/net10.0/dll.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dll")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3394a58ab105db83ea7040727b9cbdd1cb91f1c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11eb2e2819ca3b616f83c29e3e10ec182b6119d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("dll")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dll")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
